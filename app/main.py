2da383d591399692ab62392a2d2d5d4c7d68aa10
Lev <lev01111989@gmail.com>
2025-09-30 13:18:42 +0200

    Attempt2

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 import time
 import asyncio
-from typing import Awaitable, Any
 
 from iot.devices import HueLightDevice, SmartSpeakerDevice, SmartToiletDevice
 from iot.message import Message, MessageType
@@ -12,51 +11,42 @@ async def main() -> None:
     service = IOTService()
 
     # create and register a few devices
-    hue_light = HueLightDevice()
-    speaker = SmartSpeakerDevice()
-    toilet = SmartToiletDevice()
-    device_ids = await asyncio.gather(
+    hue_light, speaker, toilet = HueLightDevice(), SmartSpeakerDevice(), SmartToiletDevice()
+    hue_light_id, speaker_id, toilet_id = await asyncio.gather(
         service.register_device(hue_light),
         service.register_device(speaker),
         service.register_device(toilet),
-
     )
-    hue_light_id, speaker_id, toilet_id = device_ids
-
     await run_sequence(
         run_parallel(
-            service.send_msg(
-                Message(
-                    hue_light_id,
-                    MessageType.SWITCH_ON
-                )
-            ),
-            service.send_msg(
-                Message(
-                    speaker_id,
-                    MessageType.SWITCH_ON
-                )
-            ),
+            service.send_msg(Message(hue_light_id, MessageType.SWITCH_ON)),
+            service.send_msg(Message(speaker_id, MessageType.SWITCH_ON)),
         ),
-        service.send_msg(
-            Message(
-                speaker_id,
-                MessageType.PLAY_SONG,
-                "Rick Astley - Never Gonna Give You Up",
-            )
-        ),
+        service.send_msg(Message(speaker_id, MessageType.PLAY_SONG, "Rick Astley - Never Gonna Give You Up")),
     )
 
     # run the programs
     await run_sequence(
         run_parallel(
-            service.send_msg(Message(hue_light_id, MessageType.SWITCH_OFF)),
-            service.send_msg(Message(speaker_id, MessageType.SWITCH_OFF)),
+            service.send_msg(
+                Message(
+                    hue_light_id,
+                    MessageType.SWITCH_OFF
+                )
+            ),
+            service.send_msg(
+                Message(
+                    speaker_id,
+                    MessageType.SWITCH_OFF)
+            ),
+            service.send_msg(
+                Message(
+                    toilet_id,
+                    MessageType.FLUSH
+                )
+            ),
         ),
-        run_sequence(
-            service.send_msg(Message(toilet_id, MessageType.FLUSH)),
-            service.send_msg(Message(toilet_id, MessageType.CLEAN)),
-        ),
+        service.send_msg(Message(toilet_id, MessageType.CLEAN)),
     )
 
 
